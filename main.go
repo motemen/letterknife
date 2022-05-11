496ee3d5f433984c9c25a55edafae620f7987b9e
Hironao OTSUBO <motemen@gmail.com>
2022-05-12 01:55:13 +0900

    make testable

--- main.go
+++ main.go
@@ -18,10 +18,8 @@ import (
 	"golang.org/x/text/encoding/ianaindex"
 )
 
-var modeDebug bool
-
-func debugf(format string, args ...interface{}) {
-	if modeDebug {
+func (lk *LetterKnife) debugf(format string, args ...interface{}) {
+	if lk.ModeDebug {
 		log.Printf("debug: "+format, args...)
 	}
 }
@@ -59,6 +57,60 @@ func extensionsByType(typ string) string {
 	return ".bin"
 }
 
+var delmiter = "\n"
+
+type LetterKnife struct {
+	Delmiter string
+
+	In  io.Reader
+	Out io.Writer
+
+	ShortcutFrom    string
+	ShortcutSubject string
+	ShortcutHTML    bool
+	ShortcutPlain   bool
+
+	MatchAddress string
+	MatchHeader  string
+
+	SelectPart       string
+	SelectAttachment string
+
+	PrintContent bool
+	PrintHeader  string
+	PrintRaw     bool
+	SaveFile     bool
+
+	ModeDebug bool
+}
+
+func (l *LetterKnife) ParseFlags(args []string) error {
+	flags := flag.NewFlagSet("letterknife", flag.ExitOnError)
+
+	flags.StringVar(&l.ShortcutFrom, "from", "", "Shortcut for --match-address 'From:`<pattern>`'")
+	flags.StringVar(&l.ShortcutSubject, "subject", "", "Shortcut for --match-header 'Subject:`<pattern>`'")
+	flags.BoolVar(&l.ShortcutHTML, "html", false, "Shortcut for --select-part text/html")
+	flags.BoolVar(&l.ShortcutPlain, "plain", false, "Shortcut for --select-part text/plain")
+
+	// TODO: make multiple
+	flags.StringVar(&l.MatchAddress, "match-address", "", "Filter: address header `<header>:<pattern>` eg. \"From:*@example.com\"")
+	flags.StringVar(&l.MatchHeader, "match-header", "", "Filter: header `<header>:<pattern>` eg. \"Subject:foobar\"")
+
+	flags.StringVar(&l.SelectPart, "select-part", "", "Select: non-attachment parts by `<content-type>`")
+	flags.StringVar(&l.SelectAttachment, "select-attachment", "", "Select: attachments by `<content-type>`")
+
+	flags.BoolVar(&l.PrintContent, "print-content", true, "Action: print decoded content")
+	flags.StringVar(&l.PrintHeader, "print-header", "", "Action: print `<header>`")
+	flags.BoolVar(&l.PrintRaw, "print-raw", false, "Action: print raw input as-is")
+	flags.BoolVar(&l.SaveFile, "save-file", false, "Action: save parts as files and print their paths")
+
+	flags.BoolVar(&l.ModeDebug, "debug", false, "enable debug logging")
+
+	flags.SortFlags = false
+
+	return flags.Parse(args)
+}
+
 // --from, --subject, --html, --plain
 // --match-address From:...
 // --match-header Subject:...
@@ -70,68 +122,60 @@ func extensionsByType(typ string) string {
 // --list-parts // ???
 // --debug, --quiet // TODO
 func main() {
-	delmiter := "\n"
-	var (
-		shortcutFrom    = flag.String("from", "", "Shortcut for --match-address 'From:`<pattern>`'")
-		shortcutSubject = flag.String("subject", "", "Shortcut for --match-header 'Subject:`<pattern>`'")
-		shortcutHTML    = flag.Bool("html", false, "Shortcut for --select-part text/html")
-		shortcutPlain   = flag.Bool("plain", false, "Shortcut for --select-part text/plain")
+	l := &LetterKnife{}
+	l.Delmiter = "\n"
+	l.In = os.Stdin
+	l.Out = os.Stdout
 
-		// TODO: make multiple
-		matchAddress = flag.String("match-address", "", "Filter: address header `<header>:<pattern>` eg. \"From:*@example.com\"")
-		matchHeader  = flag.String("match-header", "", "Filter: header `<header>:<pattern>` eg. \"Subject:foobar\"")
+	err := l.ParseFlags(os.Args[1:])
+	if err != nil {
+		fatalf("%v", err)
+	}
 
-		selectPart       = flag.String("select-part", "", "Select: non-attachment parts by `<content-type>`")
-		selectAttachment = flag.String("select-attachment", "", "Select: attachments by `<content-type>`")
+	l.Run()
+}
 
-		printContent = flag.Bool("print-content", true, "Action: print decoded content")
-		printHeader  = flag.String("print-header", "", "Action: print `<header>`")
-		printRaw     = flag.Bool("print-raw", false, "Action: print raw input as-is")
-		saveFile     = flag.Bool("save-file", false, "Action: save parts as files and print their paths")
-	)
-
-	flag.BoolVar(&modeDebug, "debug", false, "enable debug logging")
-
-	flag.CommandLine.SortFlags = false
-	flag.Parse()
+func (lk *LetterKnife) Run() {
+	r := lk.In
+	w := lk.Out
 
 	// holds whole input
 	var in bytes.Buffer
 
-	msg, err := mail.ReadMessage(io.TeeReader(os.Stdin, &in))
+	msg, err := mail.ReadMessage(io.TeeReader(r, &in))
 	if err != nil {
 		fatalf("failed to read message: %v", err)
 	}
 
 	pass := true
 
-	if *shortcutFrom != "" {
-		*matchAddress = "From:" + *shortcutFrom
+	if lk.ShortcutFrom != "" {
+		lk.MatchAddress = "From:" + lk.ShortcutFrom
 	}
-	if *shortcutSubject != "" {
-		*matchHeader = "Subject:" + *shortcutSubject
+	if lk.ShortcutSubject != "" {
+		lk.MatchHeader = "Subject:" + lk.ShortcutSubject
 	}
-	if *shortcutHTML {
-		*selectPart = "text/html"
+	if lk.ShortcutHTML {
+		lk.SelectPart = "text/html"
 	}
-	if *shortcutPlain {
-		*selectPart = "text/plain"
+	if lk.ShortcutPlain {
+		lk.SelectPart = "text/plain"
 	}
 
-	if *matchAddress != "" {
-		ok, err := checkMatch(msg.Header, *matchAddress, true)
+	if lk.MatchAddress != "" {
+		ok, err := lk.checkMatch(msg.Header, lk.MatchAddress, true)
 		if err != nil {
-			fatalf("checkMatch(%s): %v", *matchAddress, err)
+			fatalf("checkMatch(%s): %v", lk.MatchAddress, err)
 		}
 		if !ok {
 			pass = false
 		}
 	}
 
-	if *matchHeader != "" {
-		ok, err := checkMatch(msg.Header, *matchHeader, false)
+	if lk.MatchHeader != "" {
+		ok, err := lk.checkMatch(msg.Header, lk.MatchHeader, false)
 		if err != nil {
-			fatalf("checkMatch(%s): %v", *matchHeader, err)
+			fatalf("checkMatch(%s): %v", lk.MatchHeader, err)
 		}
 		if !ok {
 			pass = false
@@ -154,23 +198,23 @@ func main() {
 	}
 
 	var selectedParts []*mailPart
-	if *selectPart != "" {
-		pp, err := selectParts(rootPart, *selectPart, false)
+	if lk.SelectPart != "" {
+		pp, err := lk.selectParts(rootPart, lk.SelectPart, false)
 		if err != nil {
-			log.Fatal(err)
+			fatalf("%v", err)
 		}
 		selectedParts = append(selectedParts, pp...)
 	}
 
-	if *selectAttachment != "" {
-		pp, err := selectParts(rootPart, *selectAttachment, true)
+	if lk.SelectAttachment != "" {
+		pp, err := lk.selectParts(rootPart, lk.SelectAttachment, true)
 		if err != nil {
-			log.Fatal(err)
+			fatalf("%v", err)
 		}
 		selectedParts = append(selectedParts, pp...)
 	}
 
-	if *selectPart != "" || *selectAttachment != "" {
+	if lk.SelectPart != "" || lk.SelectAttachment != "" {
 		if len(selectedParts) == 0 {
 			fatalf("selecting parts failed")
 		}
@@ -178,39 +222,39 @@ func main() {
 		selectedParts = []*mailPart{wholePart}
 	}
 
-	if *printHeader != "" || *saveFile || *printRaw {
-		*printContent = false
+	if lk.PrintHeader != "" || lk.SaveFile || lk.PrintRaw {
+		lk.PrintContent = false
 	}
 
-	if *printContent {
+	if lk.PrintContent {
 		for _, mp := range selectedParts {
-			_, err = io.Copy(os.Stdout, mp)
+			_, err = io.Copy(w, mp)
 			if err != nil {
 				fatalf("%v", err)
 			}
-			fmt.Print(delmiter)
+			fmt.Fprint(w, delmiter)
 		}
 	}
 
-	if *printRaw {
-		_, err = io.Copy(os.Stdout, &in)
+	if lk.PrintRaw {
+		_, err = io.Copy(w, &in)
 		if err != nil {
 			fatalf("%v", err)
 		}
 	}
 
-	if *printHeader != "" {
+	if lk.PrintHeader != "" {
 		for _, mp := range selectedParts {
-			s, err := mimeDecoder.DecodeHeader(mp.header.Get(*printHeader))
+			s, err := mimeDecoder.DecodeHeader(mp.header.Get(lk.PrintHeader))
 			if err != nil {
-				fatalf("decoding header %q failed: %v", *printHeader, err)
+				fatalf("decoding header %q failed: %v", lk.PrintHeader, err)
 			}
-			fmt.Print(s)
-			fmt.Print(delmiter)
+			fmt.Fprint(w, s)
+			fmt.Fprint(w, delmiter)
 		}
 	}
 
-	if *saveFile {
+	if lk.SaveFile {
 		dir, err := os.MkdirTemp("", "")
 		if err != nil {
 			fatalf("while creating temporary directory: %v", err)
@@ -244,8 +288,8 @@ func main() {
 			}
 
 			f.Close()
-			fmt.Print(f.Name())
-			fmt.Print(delmiter)
+			fmt.Fprint(w, f.Name())
+			fmt.Fprint(w, delmiter)
 		}
 	}
 }
@@ -300,8 +344,6 @@ func (m *mailPart) attachmentFilename() (string, bool) {
 	return m.dispositionParams["filename"], true
 }
 
-var _ io.Reader = (*mailPart)(nil)
-
 func buildPartTree(header mail.Header, body io.Reader) (*mailPart, error) {
 	ct := header.Get("Content-Type")
 
@@ -341,12 +383,12 @@ func buildPartTree(header mail.Header, body io.Reader) (*mailPart, error) {
 	return &part, err
 }
 
-func visitParts(mp *mailPart, visit func(*mailPart) error) error {
-	debugf("visitParts: %v sub=%v", mp.header.Get("Content-Type"), mp.subparts)
+func (lk *LetterKnife) visitParts(mp *mailPart, visit func(*mailPart) error) error {
+	lk.debugf("visitParts: %v sub=%v", mp.header.Get("Content-Type"), mp.subparts)
 
 	if mp.isMultipart() {
 		for _, p := range mp.subparts {
-			if err := visitParts(p, visit); err != nil {
+			if err := lk.visitParts(p, visit); err != nil {
 				return err
 			}
 		}
@@ -356,9 +398,9 @@ func visitParts(mp *mailPart, visit func(*mailPart) error) error {
 	return visit(mp)
 }
 
-func selectParts(mp *mailPart, mediaTypeSpec string, isAttachmentSpec bool) ([]*mailPart, error) {
+func (lk *LetterKnife) selectParts(mp *mailPart, mediaTypeSpec string, isAttachmentSpec bool) ([]*mailPart, error) {
 	parts := []*mailPart{}
-	err := visitParts(mp, func(mp *mailPart) error {
+	err := lk.visitParts(mp, func(mp *mailPart) error {
 		_, isAttachment := mp.attachmentFilename()
 		if isAttachment != isAttachmentSpec {
 			return nil
@@ -382,7 +424,7 @@ func selectParts(mp *mailPart, mediaTypeSpec string, isAttachmentSpec bool) ([]*
 	return parts, nil
 }
 
-func checkMatch(h mail.Header, in string, isAddr bool) (bool, error) {
+func (lk *LetterKnife) checkMatch(h mail.Header, in string, isAddr bool) (bool, error) {
 	// TODO: fail if header does not exist
 	p := strings.IndexByte(in, ':')
 	if p == -1 {
@@ -410,7 +452,7 @@ func checkMatch(h mail.Header, in string, isAddr bool) (bool, error) {
 			return false, err
 		}
 
-		debugf("test %s: %q against %q", header, value, pattern)
+		lk.debugf("test %s: %q against %q", header, value, pattern)
 
 		ok, err := testPattern(value, pattern)
 		if err != nil {
--- main_test.go
+++ main_test.go
@@ -1,10 +1,27 @@
 package main
 
 import (
+	"bytes"
+	"os"
 	"testing"
 
 	"gotest.tools/v3/assert"
+	"gotest.tools/v3/assert/cmp"
 )
+
+func TestRunMain(t *testing.T) {
+	f, err := os.Open("testdata/example.eml")
+	assert.NilError(t, err)
+
+	var buf bytes.Buffer
+	lk := LetterKnife{In: f, Out: &buf}
+	err = lk.ParseFlags([]string{"--plain"})
+	assert.NilError(t, err)
+
+	lk.Run()
+
+	assert.Check(t, cmp.Contains(buf.String(), "Hello! 😊"))
+}
 
 func TestRegexpFromPattern(t *testing.T) {
 	tests := []struct {
